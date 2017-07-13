a951034053cf355f3cc7820f8a8e10c9e6e5e112
Rupesh Singh <wrufesh@gmail.com>
2017-07-13 11:59:19 +0545

    not successful in test script

--- test_script.py
+++ test_script.py
@@ -2,23 +2,21 @@ import subprocess
 
 import time
 
-airodump = subprocess.Popen(['airodump-ng','prism0'],stdin=subprocess.PIPE,
+airodump = subprocess.Popen(['airodump-ng','prism0'],
                               stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE,
-                              universal_newlines=True
+                              stderr=subprocess.STDOUT,
+                              # universal_newlines=True
                             )
 
 for i in [1, 2, 3, 4 ,5]:
-    # time.sleep(10)
-    try:
-        # o_airodump, unused_stderr = airodump.communicate(timeout=10)
-    except:
-        print('hey')
-        print(o_airodump)
-        print('heyo')
-    # o_file = airodump.stdout
-    # x = o_file.read()
-    # print(x)
+    time.sleep(10)
+    # try:
+    #     # o_airodump, unused_stderr = airodump.communicate(timeout=10)
+    # except:
+    #     print('hey')
+    #     print(o_airodump)
+    #     print('heyo')
+    print(airodump.stdout.read())
     # print('One file printed')
     # # import ipdb
     # ipdb.set_trace()
